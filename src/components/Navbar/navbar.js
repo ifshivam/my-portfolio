f658cc75f6120c0bd1d5c28986869311cbcc9017
ifshivam <shivamrai0307@gmail.com>
2023-10-07 01:18:01 +0530

    navbar updated

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './navbar.css'
 import logo from '../../assets/logo.png';
 import {Link} from 'react-scroll';
+import contactImg from '../../assets/contact.png'
 const Navbar = () => {
   return (
     <nav className='navbar'>
@@ -13,7 +14,7 @@ const Navbar = () => {
             <Link className='desktopMenuListItem'>Clients</Link>
         </div>
         <button className='desktopMenuBtn'>
-            <img src="" alt="" className='desktopMenuImg'/>Contact Me</button>
+            <img src={contactImg} alt="" className='desktopMenuImg'/>Contact Me</button>
     </nav>
   )
 }
